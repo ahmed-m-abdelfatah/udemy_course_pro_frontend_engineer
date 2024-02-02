8a3cf133efec5ba8127a498f4a79066fb003e753
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2024-02-02 04:44:29 +0200

    init

---
+++ project-01-alert/src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return <div>App</div>;
+};
+
+// components
+
+export default App;
---
+++ project-01-alert/src/components/Alert.tsx
@@ -0,0 +1,7 @@
+interface AlertProps {}
+
+const Alert = ({}: AlertProps) => {
+  return <div>Alert</div>;
+};
+
+export default Alert;
