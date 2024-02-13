4e0f086bbaa4482b4e12c844173f373cdaf651fd
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2024-02-14 01:14:42 +0200

    tutorial #55

--- project-01-alert/src/components/alert/Alert.tsx
+++ project-01-alert/src/components/alert/Alert.tsx
@@ -5,19 +5,21 @@ interface AlertProps {}
 
 const Alert = ({}: AlertProps) => {
   return (
-    <div className='alert-wrapper'>
-      <BellRing />
+    <div className='alert-wrapper danger'>
+      <div className='alert-header'>
+        <div className='title'>
+          <BellRing className='icon' />
+          <h4>Title</h4>
+        </div>
 
-      <h4>Title</h4>
+        <X />
+      </div>
+
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, officia esse a recusandae quibusdam
         voluptates obcaecati qui exercitationem cum quo illum, laboriosam nobis doloremque, excepturi accusantium
         consequuntur inventore. Laudantium, at!
       </p>
-
-      <span>
-        <X />
-      </span>
     </div>
   );
 };
