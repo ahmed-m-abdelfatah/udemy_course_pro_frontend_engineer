ec40a311f6255d03cbd2c3b2e92bdf499d9694c1
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2024-03-02 00:05:52 +0200

    tutorial #64

---
+++ project-02/src/components/ProductCard.tsx
@@ -0,0 +1,7 @@
+interface ProductCardProps {}
+
+const ProductCard = ({}: ProductCardProps) => {
+  return <div className='border border-white text-white rounded-md text-sm p-4'>ProductCard</div>;
+};
+
+export default ProductCard;
