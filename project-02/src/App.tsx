a5d90e5bd98c2516cd1fad0ed9c3d1c8fc3d113e
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2024-03-12 22:43:14 +0200

    tutorial #117

--- project-02/src/App.tsx
+++ project-02/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import ProductCard from './components/ProductCard';
 import Modal from './components/ui/Modal';
-import { productList } from './data';
+import { formInputsList, productList } from './data';
 import Button from './components/ui/Button';
+import Input from './components/ui/Input';
 
 const App = () => {
   // ------------- Renders -------------
@@ -25,6 +26,15 @@ const App = () => {
   //   return productList.map(product => <ProductCard key={product.id} />);
   // };
 
+  const renderFormInputList = formInputsList.map(input => (
+    <div className='flex flex-col'>
+      <label className='mb-[2px] text-sm font-medium text-gray-700' htmlFor={input.id}>
+        {input.label}
+      </label>
+      <Input id={input.id} type={input.type} name={input.name} />
+    </div>
+  ));
+
   return (
     <main className='container mx-auto'>
       <Button className='bg-indigo-700 hover:bg-indigo-800' onClick={openModal}>
@@ -33,13 +43,19 @@ const App = () => {
 
       <div className='m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md'>
         {renderProductList}
+
+        {/* Modal */}
         <Modal isOpen={isOpen} closeModal={closeModal} title='ADD A NEW PRODUCT'>
-          <div className='flex items-center space-x-3'>
-            <Button className='bg-indigo-700 hover:bg-indigo-800'>Submit</Button>
-            <Button className='bg-gray-400 hover:bg-gray-500' width='w-fit'>
-              Cancel
-            </Button>
-          </div>
+          <form className='space-y-3'>
+            {renderFormInputList}
+
+            <div className='flex items-center space-x-3'>
+              <Button className='bg-indigo-700 hover:bg-indigo-800'>Submit</Button>
+              <Button className='bg-gray-400 hover:bg-gray-500' width='w-fit'>
+                Cancel
+              </Button>
+            </div>
+          </form>
         </Modal>
       </div>
     </main>
--- project-02/src/components/ui/Modal.tsx
+++ project-02/src/components/ui/Modal.tsx
@@ -34,7 +34,7 @@ const Modal = ({ isOpen, closeModal, title, children }: ModalProps) => {
                 leave='ease-in duration-200'
                 leaveFrom='opacity-100 scale-100'
                 leaveTo='opacity-0 scale-95'>
-                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-md bg-white p-6 text-left align-middle shadow-xl transition-all'>
                   {title && (
                     <Dialog.Title as='h3' className='text-lg font-medium leading-6 text-gray-900'>
                       {title}
